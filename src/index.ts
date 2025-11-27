69ecfbe39b41458b0af44dcc2193d2fb635a3453
katsura919 <katsuragik919@gmail.com>
2025-11-27 21:13:34 +0800

    fix cors

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ app.use(
     origin: ["http://localhost:3000"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-enforcer-id"],
   })
 );
 app.use(express.json());
